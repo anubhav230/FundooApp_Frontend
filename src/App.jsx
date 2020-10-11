9a03259f4fcb8c3292dc3dbc8049a1cfe6536146
anubhav singh <anubhavsingh230@gmail.com>
2020-10-11 22:49:33 +0530

    refactor: Router in app file

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
 import React from 'react';
 import './App.css';
-import {Route, Switch } from 'react-router-dom'
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 import LoginIn from './components/Login'
 import Error from './components/Error'
 
-
 function App() {
     return (
-        <Switch>
-            <Route exact path="/" component={LoginIn}/>
-            <Route component={Error}/>
-        </Switch>
+        <Router>
+            <Switch>
+                <Route exact path="/" component={LoginIn} />
+                <Route component={Error} />
+            </Switch>
+        </Router>
     );
 }
 
