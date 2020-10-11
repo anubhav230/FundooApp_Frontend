28fceea62f76ed94b8355e7d2f30f76cf4d3d7f9
anubhav singh <anubhavsingh230@gmail.com>
2020-10-11 22:32:38 +0530

    refactor(validation): Added formik validation properly in login page

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React from 'react';
 import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import TextField from '@material-ui/core/TextField';
@@ -15,7 +15,7 @@ import * as Yup from "yup";
 const useStyles = makeStyles((theme) => ({
 
     form: {
-        width: '100%', // Fix IE 11 issue.
+        width: '100%',
         marginTop: theme.spacing(1),
     },
     submit: {
@@ -42,86 +42,104 @@ const useStyles = makeStyles((theme) => ({
         flexDirection: 'column',
         alignItems: 'center',
     },
-
+    error: {
+        backgroundColor: "red"
+    }
 }));
 
-const validation = Yup.object().shape({
+
+const validationSchema = Yup.object().shape({
 
     email: Yup.string().email().required("Email required!"),
     password: Yup.string()
         .min(4, "Must have minimum Charachters")
-        .required("Required !").matches(/(?=.*[0-9])/, "Password must contain a number."),
+        .required("Password required!").matches(/(?=.*[0-9])/, "Password must contain a number."),
+
 });
 
-
-
 export default function SignIn() {
-
-    const [open, setOpen] = React.useState(false);
-    const [user, setUser] = useState({email : "",password : "" });
-
-
     const classes = useStyles();
     return (
-        <Formik initialValues={{email:"", password:""}} validationSchema={validation}>
-        <Container component="main" maxWidth="xs">
-            <CssBaseline />
-            <main className={classes.layout}>
-                <Paper className={classes.paper}>
-                    <div className={classes.paper}>
-                        <img src={Logo} />
-                        <Typography component="h1" variant="h5">
-                            Sign in
-                    </Typography>
-                        <form className={classes.form} noValidate>
-                            <TextField
-                                variant="outlined"
-                                margin="normal"
-                                required
-                                fullWidth
-                                id="email"
-                                label="Email Address"
-                                name="email"
-                                autoComplete="email"
-                                autoFocus
-                            />
-                            <TextField
-                                variant="outlined"
-                                margin="normal"
-                                required
-                                fullWidth
-                                name="password"
-                                label="Password"
-                                type="password"
-                                id="password"
-                                autoComplete="current-password"
-                            />
-                            <Button
-                                type="submit"
-                                fullWidth
-                                variant="contained"
-                                color="primary"
-                                className={classes.submit}
-                            >
-                                Sign In
-                        </Button>
-                            <Grid container>
-                                <Grid item xs>
-                                    <Link href="#" variant="body2">
-                                        Forgot password?
-                        </Link>
-                                </Grid>
-                                <Grid item>
-                                    <Link href="#" variant="body2">
-                                        {"Don't have an account? Sign Up"}
+        <Formik
+            initialValues={{email: "", password: ""}}
+            validationSchema={validationSchema}>
+                {({ values, errors, touched, handleChange, handleBlur, handleSubmit, isSubmitting}) => (
+                <Container component="main" maxWidth="xs">
+                    <CssBaseline />
+                    <main className={classes.layout}>
+                        <Paper className={classes.paper}>
+                            <div className={classes.paper}>
+                                <img src={Logo} />
+                                <Typography component="h1" variant="h5">
+                                    Sign in
+                                </Typography>
+                                <form className={classes.form} noValidate onSubmit={handleSubmit}>
+                                    <TextField
+                                        variant="outlined"
+                                        margin="normal"
+                                        required
+                                        fullWidth
+                                        id="email"
+                                        label="Email Address"
+                                        name="email"
+                                        autoComplete="email"
+                                        autoFocus
+                                        value={values.email}
+                                        onChange={handleChange}
+                                        onBlur={handleBlur}
+                                        className={errors.email && touched.email && "error"}
+                                    />
+                                    {errors.email && touched.email && (
+                                        <div className="input-feedback">{errors.email}</div>
+                                    )}
+                                    <TextField
+                                        variant="outlined"
+                                        margin="normal"
+                                        required
+                                        fullWidth
+                                        name="password"
+                                        label="Password"
+                                        type="password"
+                                        id="password"
+                                        autoComplete="current-password"
+                                        value={values.password}
+                                        onChange={handleChange}
+                                        onBlur={handleBlur}
+                                        className={errors.password && touched.password && "error"}
+                                    />
+                                    {errors.password && touched.password && (
+                                        <div className="input-feedback">{errors.password}</div>
+                                    )}
+                                    <Button
+                                        type="submit"
+                                        fullWidth
+                                        variant="contained"
+                                        color="primary"
+                                        className={classes.submit}
+                                        disabled={isSubmitting}
+                                    >
+                                        Sign In
+                                    </Button>
+                                    <Grid container>
+                                        <Grid item xs>
+                                            <Link href="#" variant="body2">
+                                                Forgot password?
                                     </Link>
-                                </Grid>
-                            </Grid>
-                        </form>
-                    </div>
-                </Paper>
-            </main>
-        </Container>
+                                        </Grid>
+                                        <Grid item>
+                                            <Link href="#" variant="body2">
+                                                {"Don't have an account? Sign Up"}
+                                            </Link>
+                                        </Grid>
+                                    </Grid>
+                                </form>
+                            </div>
+                        </Paper>
+                    </main>
+                </Container>
+
+            // }}
+                )}
         </Formik>
-    );
+    )
 }
